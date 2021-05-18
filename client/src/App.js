0f14c3e1b43e53d99ff8ca56749683254bdb968b
fallingsappy <novikovvlal@gmail.com>
2021-05-18 16:38:10 +0300

    initial

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>Fib Calculator v2</div>
+          <Link style={{ color: 'white' }} to="/">Fib values</Link>
+          <Link style={{ color: 'white' }} to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
